6792c8d39f8f675603ca2e13b00a26b8164240ee
Tonoaki Sato <post@aramashi.jp>
2018-01-16 18:39:22 +0900

    編集

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,11 +15,10 @@
                     @endif
 
                     <div>
-                      <a href="{{ route('minutes') }}" class="stech_btn">議事録
+                      <a href="{{ route('minutes') }}">議事録
                       </a>
                     </div>
-                    You are logged in!<br />
-                    ・議事録（一覧、読む、作る）<br />
+                    <br />
                     ・メーリングリスト（一覧、読む）<br />
                     ・スケジュール（一覧、見る、作る）<br />
                     ・組織図（見る、作る）
