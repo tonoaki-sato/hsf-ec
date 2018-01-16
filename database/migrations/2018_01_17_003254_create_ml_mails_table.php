b08eba7c0c81e4f7ebed11db72df8b55df7f1318
Tonoaki Sato <post@aramashi.jp>
2018-01-17 00:48:03 +0900

    edit

---
+++ database/migrations/2018_01_17_003254_create_ml_mails_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMlMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ml_mails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('h_message_id', 100);
+            $table->dateTime('h_date');
+            $table->string('h_from');
+            $table->string('h_subject');
+            $table->text('header');
+            $table->text('contents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ml_mails');
+    }
+}
