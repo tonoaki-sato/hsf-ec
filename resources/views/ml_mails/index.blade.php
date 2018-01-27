d4cc7bc8397bd4e34f6a75dae51d79f1a63f6f5b
Tonoaki Sato <post@aramashi.jp>
2018-01-27 18:26:34 +0900

    表示内容追加

--- resources/views/ml_mails/index.blade.php
+++ resources/views/ml_mails/index.blade.php
@@ -22,6 +22,10 @@
             <li class="list-group-item d-flex justify-content-between align-items-center">
               <div>{{ $element->h_subject }}</div>
               <nav class="nav">
+                <div>
+                {{ str_replace("<" . env('MAIL_FROM_ADDRESS') . ">", "", $element->h_from) }}
+                <span style="padding-left: 5px; font-size: 12px;">{{ date("Y年m月d日 H:i:s", strtotime($element->h_date)) }}</span>
+                </div>
                 <a class="nav-link" href="{{ route('ml_mails.show', ['id' => $element->id]) }}">読む</a>
               </nav>
             </li>
